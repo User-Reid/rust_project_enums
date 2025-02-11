d28857b3293b94968260d4761ce3f4bcad32cb96
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-10 21:38:31 -0600

    revert

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,21 @@ enum Subscription {
     },
 }
 
+impl Subscription {
+    fn summarize(self) {
+        match self {
+            Subscription::Free => {
+                println!("You have limited access to the site")
+            },
+            Subscription::Basic(price, months) => {
+                println!("You have limited access to the site's premium features for {price} for {months} months.")
+            },
+            Subscription::Premium { tier } => {
+                println!("You have full access to the site's premium features. Your tier is {tier:?}")
+            }
+        }
+    }
+}
 
 fn main() {
     Subscription::Free.summarize();
